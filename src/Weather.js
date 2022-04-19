c9ebbfb5e13d004f6a0bcb31c3d2f5ce6c8a2f17
brennawilliamson <91994168+brennawilliamson@users.noreply.github.com>
2022-04-19 14:25:06 -0400

    Added weather app pages, components and css

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,78 @@
 import React from "react";
-import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in in ${response.data.name} is ${response.data.main.temp} `
-    );
-  }
+import "./Weather.css";
 
-  let units = "imperial";
-  let apiKey = "ec69812db7fd144177914635cab17886";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units} `;
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    temperature: 19,
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
 
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return (
+    <div className="Weather">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <form>
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="search"
+                  placeholder="Type a city..."
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="col-2">
+                <button type="submit" className="btn btn-light">
+                  Search
+                </button>
+              </div>
+              <div className="col-4">
+                <button type="submit" className="btn btn-outline-light ms-2">
+                  Current
+                </button>
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <h2>
+                <span>Last updated: {weatherData.date}</span>
+              </h2>
+              <h3>{weatherData.description}</h3>
+            </ul>
+          </div>
+          <div className="row">
+            <div className="col-6">
+              <div className="d-flex weather-temperature">
+                <img
+                  src={weatherData.imgUrl}
+                  alt={weatherData.description}
+                  className="float-left"
+                />
+                <div className="float-left">
+                  <strong>{weatherData.temperature}</strong>
+                  <span className="units"> °F</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind} mph</li>
+                <li>Feels Like: 60°F</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
